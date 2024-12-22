1bc56234902f276b10cd4b1c8539049301592ffb
Silahanim21 <turkcell63632@gmail.com>
2024-12-22 23:01:26 +0300

    Update botwelcomlog.py

--- AlexaMusic/plugins/modules/botwelcomlog.py
+++ AlexaMusic/plugins/modules/botwelcomlog.py
@@ -1,30 +1,52 @@
-# This code is written by (C) TheTeamAlexa bot will send message to log group when someone add
-# this bot to new group make sure to star all projects
-# Copyright (C) 2021-2024 by Alexa_Help@ Github, < TheTeamAlexa >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
-# All rights reserved. © Alisha © Alexa © Yukki
-
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from AlexaMusic import app
-from AlexaMusic.utils.database import get_served_chats
 from config import LOG_GROUP_ID
+from AlexaMusic import app
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-async def lul_message(chat_id: int, message: str):
-    await app.send_message(chat_id=chat_id, text=message)
+async def new_message(chat_id: int, message: str, reply_markup=None):
+    await app.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup)
 
 
 @app.on_message(filters.new_chat_members)
 async def on_new_chat_members(client: Client, message: Message):
     if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
-        added_by = message.from_user.first_name if message.from_user else "ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ"
-        matlabi_jhanto = message.chat.title
-        served_chats = len(await get_served_chats())
+        added_by = f"{message.from_user.first_name}"
+        chatusername = f"@{message.chat.username}"
+        title = message.chat.title
         chat_id = message.chat.id
-        if message.chat.username:
-            chatusername = f"@{message.chat.username}"
-        else:
-            chatusername = "Özel Sohbet"
-        lemda_text = f"🌹 Bot yeni bir gruba eklendi ..\n\n┏━━━━━━━━━━━━━━━━━┓\n┣★ **Sohbet** › : {matlabi_jhanto}\n┣★ **Sohbet ID** › : {chat_id}\n┣★ **Sohbet Kullanıcı Adı** › : {chatusername}\n┣★ **Toplam Sohbet** › : {served_chats}\n┣★ **Tarafından Eklendi** › :\n┗━━━ {added_by}"
-        await lul_message(LOG_GROUP_ID, lemda_text)
+        sigma = f" <u>#**Yeni Gruba Eklendi**</u> :\n\n**Grup ID:** {chat_id}\n**Grup Adı:** {title}\n**Grup Link:** {chatusername}\n**Gruba Ekleyen:** {added_by}"
+
+        reply_markup = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, user_id=message.from_user.id
+                    )
+                ]
+            ]
+        )
+
+        await new_message(LOG_GROUP_ID, sigma, reply_markup)
+
+
+@app.on_message(filters.left_chat_member)
+async def on_left_chat_member(client: Client, message: Message):
+    if (await client.get_me()).id == message.left_chat_member.id:
+        removed_by = f"{message.from_user.first_name}"
+        title = message.chat.title
+        chat_id = message.chat.id
+        bye = f" <u>#**Gruptan Çıkarıldı**</u> :\n\n**Grup ID:** {chat_id}\n**Grup Adı:** {title}\n**Gruptan Çıkaran:** {removed_by}"
+
+        reply_markup = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, user_id=message.from_user.id
+                    )
+                ]
+            ]
+        )
+
+        await new_message(LOG_GROUP_ID, bye, reply_markup)
