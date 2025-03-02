b03b3a7c510399a9a7488473c401d724f3f1477f
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2025-03-02 15:38:34 +0300

    Update thumbnails.py

--- AlexaMusic/utils/thumbnails.py
+++ AlexaMusic/utils/thumbnails.py
@@ -90,7 +90,7 @@ async def gen_thumb(videoid):
         name_font = ImageFont.truetype("assets/font.ttf", 30)
         para = textwrap.wrap(title, width=30)
         j = 0
-        draw.text((5, 5), f"Alexa MusicBot", fill="white", font=name_font)
+        draw.text((5, 5), f"Kumsal music tr", fill="white", font=name_font)
         draw.text(
             (600, 150),
             "yayın başladı",
