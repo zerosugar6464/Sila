74e5d9cfbb80fb00ff8847558a0dd96afe7154b1
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2025-03-04 17:03:59 +0300

    Update help.py

--- AlexaMusic/utils/inline/help.py
+++ AlexaMusic/utils/inline/help.py
@@ -26,34 +26,36 @@ def help_pannel(_, START: Union[bool, int] = None):
         ),
         InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data=f"close"),
     ]
-    mark = second if START else first
+        mark = second if START else first
     upl = InlineKeyboardMarkup(
         [
             [
+                InlineKeyboardButton(
+                    text=_["H_B_1"],
+                    callback_data="help_callback hb1",
+                ),
                 InlineKeyboardButton(
                     text=_["H_B_2"],
                     callback_data="help_callback hb2",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_1"],
-                    callback_data="help_callback hb1",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
                     text=_["H_B_3"],
                     callback_data="help_callback hb3",
                 ),
+            ],
+            [
                 InlineKeyboardButton(
                     text=_["H_B_4"],
                     callback_data="help_callback hb4",
                 ),
-            ],
-            [
+                InlineKeyboardButton(
+                    text=_["H_B_5"],
+                    callback_data="help_callback hb5",
+                ),
                 InlineKeyboardButton(
                     text=_["H_B_6"],
-                    callback_data="help_callback hb5",
-               ),
+                    callback_data="help_callback hb6",
+                ),
             ],
             [
                 InlineKeyboardButton(
@@ -64,12 +66,10 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_8"],
                     callback_data="help_callback hb8",
                 ),
-             ],
-             [
                 InlineKeyboardButton(
                     text=_["H_B_9"],
                     callback_data="help_callback hb9",
-                  ),
+                ),
             ],
             mark,
         ]
