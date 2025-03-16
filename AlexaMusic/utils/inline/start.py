e7a2d36a6c8df750420ea29df3a58a1e21ec8771
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2025-03-16 17:36:39 +0300

    Update start.py

--- AlexaMusic/utils/inline/start.py
+++ AlexaMusic/utils/inline/start.py
@@ -23,9 +23,7 @@ def start_pannel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?start=help",
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="settings_helper"),
+                url=f"https://t.me/{app.username}?start=help",            
         ],
     ]
     if SUPPORT_CHANNEL and SUPPORT_GROUP:
