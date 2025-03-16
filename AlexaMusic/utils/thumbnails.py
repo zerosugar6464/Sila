f58889657e0e141fa55b835f9453ab4c42317686
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2025-03-16 16:49:15 +0300

    Update thumbnails.py

--- AlexaMusic/utils/thumbnails.py
+++ AlexaMusic/utils/thumbnails.py
@@ -40,13 +40,7 @@ async def gen_thumb(videoid):
     try:
         results = VideosSearch(url, limit=1)
         for result in (await results.next())["result"]:
-            try:
-                title = result["title"]
-                title = re.sub("\W+", " ", title)
-                title = title.title()
-            except:
-                title = "Unsupported Title"
-            try:
+            try:                
                 duration = result["duration"]
             except:
                 duration = "Unknown Mins"
