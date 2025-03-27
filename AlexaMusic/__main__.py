527ad126722714c2288d019d321f116237f3fb2a
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2025-03-27 22:17:29 +0300

    Update __main__.py

--- AlexaMusic/__main__.py
+++ AlexaMusic/__main__.py
@@ -1,17 +1,18 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
+# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
 # Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
 
-""""
+"""
 TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
+Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
 
 This program is free software: you can redistribute it and can modify
 as you want or you can collabe if you have new ideas.
 """
 
 
+import asyncio
 import importlib
-import sys
+from typing import Any
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
@@ -25,39 +26,32 @@ from AlexaMusic.plugins import ALL_MODULES
 from AlexaMusic.utils.database import get_banned_users, get_gbanned
 
 
-async def init():
-    if (
-        not config.STRING1
-        and not config.STRING2
-        and not config.STRING3
-        and not config.STRING4
-        and not config.STRING5
-    ):
+async def init() -> None:
+    # Check for at least one valid Pyrogram string session
+    if all(not getattr(config, f"STRING{i}") for i in range(1, 6)):
         LOGGER("AlexaMusic").error("Add Pyrogram string session and then try...")
-        sys.exit()
+        exit()
     await sudo()
     try:
-        users = await get_gbanned()
-        for user_id in users:
+        for user_id in await get_gbanned():
             BANNED_USERS.add(user_id)
-        users = await get_banned_users()
-        for user_id in users:
+        for user_id in await get_banned_users():
             BANNED_USERS.add(user_id)
     except:
         pass
     await app.start()
-    for all_module in ALL_MODULES:
-        importlib.import_module("AlexaMusic.plugins" + all_module)
+    for module in ALL_MODULES:
+        importlib.import_module("AlexaMusic.plugins" + module)
     LOGGER("AlexaMusic.plugins").info("Necessary Modules Imported Successfully.")
     await userbot.start()
     await Alexa.start()
     try:
         await Alexa.stream_call("https://telegra.ph/file/b60b80ccb06f7a48f68b5.mp4")
-    except (NoActiveGroupCall):
+    except NoActiveGroupCall:
         LOGGER("AlexaMusic").error(
             "[ERROR] - \n\nTurn on group voice chat and don't put it off otherwise I'll stop working thanks."
         )
-        sys.exit()
+        exit()
     except:
         pass
     await Alexa.decorators()
@@ -69,5 +63,5 @@ async def init():
 
 
 if __name__ == "__main__":
-    app.run(init())
+    asyncio.get_event_loop().run_until_complete(init())
     LOGGER("AlexaMusic").info("Stopping Music Bot")
